c4df9379ea51faacc345bb3fd8e36b4824491a86
Miguel González <miguel.gonzalez@sngular.com>
2019-05-15 09:23:53 +0200

    counter time

--- counter/index.js
+++ counter/index.js
@@ -12,3 +12,23 @@ Rx.Observable.fromEvent(buttonRxjs, 'click')
     .scan(count => count + 1, 0)
     // Subcribe 
     .subscribe(count => console.log(`RxJS click ${count} times`));
+
+
+var count = 0;
+var rate = 1000;
+var lastClick = Date.now() - rate;
+var button = document.getElementById('JSButton');
+button.addEventListener('click', (event) => {
+  if (Date.now() - lastClick >= rate) {
+    count += event.clientX;
+    console.log(count)
+    lastClick = Date.now();
+  }
+});
+
+var button = document.getElementById('RxJSButton');
+Rx.Observable.fromEvent(button, 'click')
+  .throttle(1000)
+  .map(event => event.clientX)
+  .scan((count, clientX) => count + clientX, 0)
+  .subscribe(count => console.log(count));
